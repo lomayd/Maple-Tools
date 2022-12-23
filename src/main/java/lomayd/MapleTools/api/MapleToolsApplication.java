0c0f3ca8c9babae46d026bf39789614d6efc0f21
lomayd <jinho10167@gmail.com>
2022-12-23 22:15:05 +0900

    feat: Project Initialize

---
+++ src/main/java/lomayd/MapleTools/api/MapleToolsApplication.java
@@ -0,0 +1,13 @@
+package lomayd.MapleTools.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapleToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapleToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lomayd/MapleTools/MapleToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package lomayd.MapleTools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapleToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
